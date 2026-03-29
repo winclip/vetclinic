0d2d160af3a089460783900289805bfd68ca5afd
Sergei Okhotnikov <sokhotnikoff@gmail.com>
2026-03-29 12:24:20 +0200

    add User entity, UserRole enum,  UserRepository

---
+++ src/main/java/dev/winclip/vetclinic/user/User.java
@@ -0,0 +1,35 @@
+package dev.winclip.vetclinic.user;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false, unique = true, length = 64)
+	private String username;
+
+	@Column(name = "password_hash", nullable = false, length = 255)
+	private String passwordHash;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false, length = 32)
+	private UserRole role;
+}
---
+++ src/main/java/dev/winclip/vetclinic/user/UserRole.java
@@ -0,0 +1,6 @@
+package dev.winclip.vetclinic.user;
+
+public enum UserRole {
+	ADMIN,
+	USER
+}
