ce915f57160580c91b83c2d5d0dc535be3e5419f
Sergei Okhotnikov <sokhotnikoff@gmail.com>
2026-04-08 14:01:37 +0200

    add swagger examples

--- src/main/java/dev/winclip/vetclinic/auth/RegisterRequest.java
+++ src/main/java/dev/winclip/vetclinic/auth/RegisterRequest.java
@@ -1,13 +1,13 @@
 package dev.winclip.vetclinic.auth;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record RegisterRequest(
-		@NotBlank @Size(min = 4, max = 64) String username,
-		@NotBlank @Size(min = 8, max = 128) String password,
-		@NotBlank @Email @Size(max = 255) String email,
-		@NotBlank @Size(max = 200) String fullName
-) {
+		@NotBlank @Size(min = 4, max = 64) @Schema(example = "john_wick") String username,
+		@NotBlank @Size(min = 8, max = 128) @Schema(example = "top_secret_password") String password,
+		@NotBlank @Email @Size(max = 255) @Schema(example = "john.wick@example.com") String email,
+		@NotBlank @Size(max = 200) @Schema(example = "John Wick") String fullName) {
 }
--- src/main/java/dev/winclip/vetclinic/user/dto/UserPasswordChangeRequest.java
+++ src/main/java/dev/winclip/vetclinic/user/dto/UserPasswordChangeRequest.java
@@ -1,10 +1,10 @@
 package dev.winclip.vetclinic.user.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record UserPasswordChangeRequest(
-		@NotBlank String currentPassword,
-		@NotBlank @Size(min = 8, max = 128) String newPassword
-) {
+		@NotBlank @Schema(example = "top_secret_password") String currentPassword,
+		@NotBlank @Size(min = 8, max = 128) @Schema(example = "baba-yaga-top") String newPassword) {
 }
